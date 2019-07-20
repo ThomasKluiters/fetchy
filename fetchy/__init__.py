9a1f9951d2a53754a3fb290af189adf280f34d61
thomas <thomas.kluiters@gmail.com>
2019-07-20 14:15:38 +0200

    Implement packages in Parser

--- fetchy/__init__.py
+++ fetchy/__init__.py
@@ -1,3 +1,11 @@
 __version__ = "0.1.0"
 
+from .version import Version, version_from_string
+from .dependency import (
+    Dependency,
+    dependency_from_string,
+    relationship_from_string,
+    dependencies_from_string,
+)
+from .package import Package, package_from_dict
 from .parser import Parser
--- fetchy/parser.py
+++ fetchy/parser.py
@@ -1,3 +1,6 @@
+from fetchy import package_from_dict
+
+
 class Parser(object):
     def __init__(self, packages_file, fields=None):
         """The Control File Parser
@@ -20,7 +23,19 @@ class Parser(object):
         self.packages_file = packages_file
         self.fields = fields
 
-    def parse_packages_raw(self, fp):
+    def _parse_packages_raw(self, fp):
+        """Function that yields packages as dictionaries
+
+        This function will fully consume the
+        file and yield each package as a dictionary.
+
+        The dictionary will be populated with fields
+        that are supplied to this Parsers' Constructor.
+
+        Note that this function will work as a generator,
+        if you want to fetch all packages at once, use
+        `list(_parse_packages_raw)`.
+        """
         pkg = {}
         for line in fp:
             # New packages are introduced with whitespaces
@@ -35,7 +50,19 @@ class Parser(object):
                     break
 
     def parse(self):
+        """Function that parses all packages
+
+        This function will parse all packages into
+        a `Package` object. Returning a dictionary
+        where each package is stored with its' name
+        as a key.        
+
+        Note that each `Package` object will only
+        be populated with the fields that are
+        supplied to this Parsers' Constructor.
+        """
         pkgs = {}
         with open(self.packages_file, "r") as fp:
-            for pkg in self.parse_packages_raw(fp):
-                pkgs[pkg["Package"]] = pkg
+            for pkg in self._parse_packages_raw(fp):
+                pkgs[pkg["Package"]] = package_from_dict(pkg)
+        return pkgs
