31810320c734a2737a69374be1743598ef252611
thomas <thomas.kluiters@gmail.com>
2019-07-20 21:02:51 +0200

    Reformat tests

--- tests/test_dependency.py
+++ tests/test_dependency.py
@@ -3,22 +3,29 @@ import pytest
 from fetchy import relationship_from_string, dependency_from_string
 
 
-@pytest.mark.parametrize("input,relationship,version", [
-    ("(>= 2:12-alpha-3.2)", ">=", "12-alpha"),
-    ("(     = 3   )", "=", "3"),
-    ("(= 3)", "=", "3")
-])
+@pytest.mark.parametrize(
+    "input,relationship,version",
+    [
+        ("(>= 2:12-alpha-3.2)", ">=", "12-alpha"),
+        ("(     = 3   )", "=", "3"),
+        ("(= 3)", "=", "3"),
+    ],
+)
 def test_relationship(input, relationship, version):
     result = relationship_from_string(input)
 
     assert result.relationship == relationship
     assert result.version.upstream_version == version
 
-@pytest.mark.parametrize("input,name", [
-    ("python3", "python3"),
-    ("python3 (= 3)", "python3"),
-    ("python3 [] (= 3)", "python3")
-])
+
+@pytest.mark.parametrize(
+    "input,name",
+    [
+        ("python3", "python3"),
+        ("python3 (= 3)", "python3"),
+        ("python3 [] (= 3)", "python3"),
+    ],
+)
 def test_dependency_name(input, name):
     result = dependency_from_string("", input)
 
--- tests/test_version.py
+++ tests/test_version.py
@@ -3,13 +3,16 @@ import pytest
 from fetchy import Version, version_from_string
 
 
-@pytest.mark.parametrize("input,epoch,upstream,debian", [
-    ("42:1.0.0-alpha-3.2~", 42, "1.0.0-alpha", "3.2~"),
-    ("42:1.0.0-alpha-3.2", 42, "1.0.0-alpha", "3.2"),
-    ("42:1.0.0-3.2", 42, "1.0.0", "3.2"),
-    ("42:1.0+2-3.2", 42, "1.0+2", "3.2"),
-    ("1", 0, "1", "0")
-])
+@pytest.mark.parametrize(
+    "input,epoch,upstream,debian",
+    [
+        ("42:1.0.0-alpha-3.2~", 42, "1.0.0-alpha", "3.2~"),
+        ("42:1.0.0-alpha-3.2", 42, "1.0.0-alpha", "3.2"),
+        ("42:1.0.0-3.2", 42, "1.0.0", "3.2"),
+        ("42:1.0+2-3.2", 42, "1.0+2", "3.2"),
+        ("1", 0, "1", "0"),
+    ],
+)
 def test_version_from_string(input, epoch, upstream, debian):
     result = version_from_string(input)
 
@@ -17,11 +20,15 @@ def test_version_from_string(input, epoch, upstream, debian):
     assert result.debian_revision == debian
     assert result.epoch == epoch
 
-@pytest.mark.parametrize("input, expected", [
-    (Version("1.0.0-alpha", "3.2~", 42), "42:1.0.0-alpha-3.2~"),
-    (Version("1.0.0-alpha", "3.2", 42), "42:1.0.0-alpha-3.2"),
-    (Version("1.0.0", "3.2", 42), "42:1.0.0-3.2"),
-    (Version("1.0+2", "3.2", 42), "42:1.0+2-3.2")
-])
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (Version("1.0.0-alpha", "3.2~", 42), "42:1.0.0-alpha-3.2~"),
+        (Version("1.0.0-alpha", "3.2", 42), "42:1.0.0-alpha-3.2"),
+        (Version("1.0.0", "3.2", 42), "42:1.0.0-3.2"),
+        (Version("1.0+2", "3.2", 42), "42:1.0+2-3.2"),
+    ],
+)
 def test_version_to_str(input, expected):
     assert str(input) == expected
