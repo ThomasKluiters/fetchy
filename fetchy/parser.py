153be358b97d09f7b097d7820711547acd3d084e
thomas <thomas.kluiters@gmail.com>
2019-07-20 14:38:14 +0200

    Refactor Parser class

--- fetchy/parser.py
+++ fetchy/parser.py
@@ -24,32 +24,6 @@ class Parser(object):
         self.fields = fields
         self.pkgs = {}
 
-    def _parse_packages_raw(self, fp):
-        """Function that yields packages as dictionaries
-
-        This function will fully consume the
-        file and yield each package as a dictionary.
-
-        The dictionary will be populated with fields
-        that are supplied to this Parsers' Constructor.
-
-        Note that this function will work as a generator,
-        if you want to fetch all packages at once, use
-        `list(_parse_packages_raw)`.
-        """
-        pkg = {}
-        for line in fp:
-            # New packages are introduced with whitespaces
-            if line.startswith("\r\n") or line.startswith("\n"):
-                if pkg:
-                    yield pkg
-                    pkg = {}
-
-            for field in self.fields:
-                if line.startswith(field):
-                    pkg[field] = line.rstrip()[(len(field) + 2) :]
-                    break
-
     def parse(self):
         """Function that parses all packages
 
@@ -67,6 +41,15 @@ class Parser(object):
 
         self.pkgs = {}
         with open(self.packages_file, "r") as fp:
-            for pkg in self._parse_packages_raw(fp):
-                self.pkgs[pkg["Package"]] = package_from_dict(pkg)
-        return self.pkgs
+            pkg = {}
+            for line in fp:
+                # New packages are introduced with whitespaces
+                if line.startswith("\r\n") or line.startswith("\n"):
+                    if pkg:
+                        self.pkgs[pkg["Package"]] = package_from_dict(pkg)
+                        pkg = {}
+
+                for field in self.fields:
+                    if line.startswith(field):
+                        pkg[field] = line.rstrip()[(len(field) + 2) :]
+                        break
